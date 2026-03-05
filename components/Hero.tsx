457864877fb95fb6434c24cce447241d0a652573
lnss7 <novellysimaolucas@gmail.com>
2026-03-05 15:59:00 -0300

    Add new SVG icons for various features
    - Added 's.svg' for feature representation.
    - Introduced 'stream.svg' for streaming functionality.
    - Created 'streamName.svg' for naming streams.
    - Added 'tail.svg' for tailing logs or data.
    - Introduced 'three.svg' for a three-part representation.
    - Created 'ts.svg' for TypeScript representation.
    - Added 'twit.svg' for Twitter integration.
    - Introduced 'wha.svg' for WhatsApp integration.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,63 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
-
+import { cn } from "@/utils/cn";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
       <div>
-        <Spotlight className="-top-40 -left-10 
-        md:-left-32 md:-top-20 h-screen" fill="white"/>
-        <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple"/>
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue"/>
+        <Spotlight
+          className="-top-40 -left-10 
+        md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div className="absolute flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 top-0 left-0">
+        <div
+          className={cn(
+            "absolute inset-0 opacity-40",
+            "[background-size:40px_40px]",
+            "[background-image:linear-gradient(to_right,#f1f5f9_1px,transparent_1px),linear-gradient(to_bottom,#f1f5f9_1px,transparent_1px)]",
+            "dark:[background-image:linear-gradient(to_right,#1f2937_1px,transparent_1px),linear-gradient(to_bottom,#1f2937_1px,transparent_1px)]",
+          )}
+        />
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </h2>
+
+          <TextGenerateEffect 
+          className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          words="Crafting Dynamic Web Experiences with Next.js"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-xl lg:text-2xl">
+            Opa, me chamo Lucas Simão, sou um desenvolvedor web apaixonado por criar experiências digitais incríveis!
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="show my work"
+              icon={<FaLocationArrow/>}
+              position="right"
+            />
+          </a>
+
+        </div>
       </div>
     </div>
   );
