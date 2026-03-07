45fba3d1f704fc38a679069bca7c0ad05f155e2e
lnss7 <novellysimaolucas@gmail.com>
2026-03-06 21:02:23 -0300

    feat: Add new UI components including a 3D globe, gradient background, and Bento Grid.

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/utils/cn";
+import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -55,33 +57,69 @@ export const BentoGridItem = ({
       }}
     >
 
-      <div className={`${id===6} && 'flex justify-center h-full`}>
+      <div className={`${id === 6} && 'flex justify-center h-full`}>
         <div className="w-full h-full absolute">
           {img && (
-            <img 
+            <img
               src={img}
               alt={img}
               className={cn(imgClassName, 'object-cover object-center')}
             />
           )}
         </div>
-        <div className={`absolute right-0 -bottom-0 ${id===5 && 'w-full opacity-80'}`}>
+        <div className={`absolute right-0 -bottom-0 ${id === 5 && 'w-full opacity-80'}`}>
           {spareImg && (
-            <img 
+            <img
               src={spareImg}
               alt={spareImg}
               className={'object-cover object-center w-full h-full'}
             />
           )}
         </div>
-      </div>
 
-      <div className="transition duration-200 group-hover/bento:translate-x-2">
-        <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
-          {title}
-        </div>
-        <div className="font-sans text-xs font-normal text-neutral-600 dark:text-neutral-300">
-          {description}
+        {id === 6 && (
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
+          </BackgroundGradientAnimation>
+        )}
+
+
+
+        <div className={cn(
+          titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+        )}>
+          <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
+            {description}
+          </div>
+          <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
+            {title}
+          </div>
+
+          {id === 2 && <GlobeDemo />}
+
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex flex-col gap-3 lg:gap-8">
+                {['React.js', 'Next.js', 'TypeScript'].map((item) => (
+                  <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                    {item}
+                  </span>
+                ))}
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
+              </div>
+
+
+              <div className="flex flex-col gap-3 lg:gap-8">
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
+                {['Java', 'Python', 'GoLang'].map((item) => (
+                  <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+
         </div>
       </div>
     </div>
