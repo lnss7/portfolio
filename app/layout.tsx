35175e6147c253dbaf8248cc7f5cd9912abda606
lnss7 <novellysimaolucas@gmail.com>
2026-03-09 15:58:31 -0300

    feat: add smooth scroll with lenis

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./theme-provider";
+import { SmoothScrolling } from "@/components/SmoothScrolling";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,7 +27,9 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <SmoothScrolling>
+              {children}
+            </SmoothScrolling>
           </ThemeProvider>
       </body>
     </html>
